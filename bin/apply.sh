ba1b754aecb9cc79ab161d05a9adfcfff36e8317
Todd Walters <waltodders@gmail.com>
2025-04-12 09:54:33 -0400

    Update apply.sh to use zsh and enhance script documentation

--- bin/apply.sh
+++ bin/apply.sh
@@ -1,2 +1,5 @@
-#!/bin/sh
-ansible-playbook -i "localhost," -c local ansible_osx.yml --ask-become-pass
+#!/bin/zsh
+# This script is used to apply the mac-dev-playbook to a macOS system.
+# It activates up a virtual environment and runs the playbook.
+source ~/ansible-env/bin/activate
+ansible-playbook main.yml --ask-become-pass
